b4ce3b28563c9d81650accf290fe6e17866c7b2f
Jason Buckenham <jbuckenham@mycwt.com>
2022-05-05 15:22:51 +0100

    add ping controllers to expand span trace

---
+++ src/PingApi1/Controllers/PingController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry;
+using System.Diagnostics;
+
+namespace PingApi1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        using var source = new ActivitySource("ExampleTracer");
+
+        // A span
+        using var activity = source.StartActivity("Call to Ping API 2");
+
+        Baggage.Current.SetBaggage("ExampleItem", "Baggage information to pass through span");
+
+        // 'Ping' API 2
+        using var client = new HttpClient();
+        _ = await client.GetAsync("http://localhost:5192/ping");
+
+        // Another span
+        using var activityTwo = source.StartActivity("Arbitrary 10ms delay");
+        await Task.Delay(10);
+
+        return Ok();
+    }
+}
---
+++ src/PingApi2/Controllers/PingController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry;
+using System.Diagnostics;
+
+namespace PingApi2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var infoFromContext = Baggage.Current.GetBaggage("ExampleItem");
+
+        using var source = new ActivitySource("ExampleTracer");
+
+        // A span
+        using var activity = source.StartActivity("In Ping API 2 using GET method");
+        activity?.SetTag("InfoPingApi2Received", infoFromContext);
+        return Ok();
+    }
+}
